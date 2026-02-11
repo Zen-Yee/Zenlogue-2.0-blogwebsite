e7b25ce4163d95b0a6d69994576f4551216f8b47
Zen Yee <qhui.yee@gmail.com>
2026-02-11 17:57:04 +0800

    Update explanation for Authmiddleware in /logout

--- modules/auth/auth.routes.js
+++ modules/auth/auth.routes.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+
+// logout route should only be accessed by whoever that ALREADY LOG IN, so before redirect to the log out page, it route to the authMiddleware first to check if the user is authenticated (log in).
 router.post("/logout", authMiddleware, authController.logout);
 
 export default router;
